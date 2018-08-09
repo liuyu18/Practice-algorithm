c9a967b997ec812383cbec9e3751a8b9c4b384b3
liuyu18 <574300833@qq.com>
2018-08-09 21:11:40 +0800

    更换Linux

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -2,10 +2,50 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
-
+void printArray(int *pArr, int length) {
+	for (int i = 0; i < length; i++)
+	{
+		printf("%d ", pArr[i]);
+	}
+	printf("\n");
+}
+//Ã°ÅÝÅÅÐò·¨
+bool bubbleSort(int *pArr, int arrLen, int *pTar) {
+	if (NULL == pArr)
+	{
+		return false;
+	}
+	if (NULL == pTar)
+	{
+		return false;
+	}
+	for (int i = 0; i < arrLen; i++)
+	{
+		pTar[i] = pArr[i];
+	}
+	for (int i = 0; i < arrLen-1; i++)
+	{
+		for (int j = 0; j < arrLen-1-i; j++)
+		{
+			if (pTar[j] > pTar[j+1])
+			{
+				int tmp = pTar[j];
+				pTar[j] = pTar[j + 1];
+				pTar[j + 1] = tmp;
+			}
+			printArray(pTar, arrLen);
+		}
+	}
+	return true;
+}
 int main()
 {
+	int arr[5] = { 6, 5, 4, 3, 2 };
+	int tar[5] = { 0 };
+	bubbleSort(arr, 5, tar);
+	system("pause");
     return 0;
 }
 
