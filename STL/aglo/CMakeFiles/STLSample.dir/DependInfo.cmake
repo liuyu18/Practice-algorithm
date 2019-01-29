7c22cc885dd549b42ed35be9f858b50b6d7a13c4
ubuntu <ubuntu@localhost.localdomain>
2019-01-29 10:57:13 +0800

    Linux 编译结果一样

--- STL/aglo/CMakeFiles/STLSample.dir/DependInfo.cmake
+++ STL/aglo/CMakeFiles/STLSample.dir/DependInfo.cmake
@@ -4,11 +4,11 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/yumaowangluokejiyouxiangongsi/Desktop/Practice-algorithm/STL/aglo/TemplateClass.cpp" "/Users/yumaowangluokejiyouxiangongsi/Desktop/Practice-algorithm/STL/aglo/CMakeFiles/STLSample.dir/TemplateClass.cpp.o"
-  "/Users/yumaowangluokejiyouxiangongsi/Desktop/Practice-algorithm/STL/aglo/Tool.cpp" "/Users/yumaowangluokejiyouxiangongsi/Desktop/Practice-algorithm/STL/aglo/CMakeFiles/STLSample.dir/Tool.cpp.o"
-  "/Users/yumaowangluokejiyouxiangongsi/Desktop/Practice-algorithm/STL/aglo/main.cpp" "/Users/yumaowangluokejiyouxiangongsi/Desktop/Practice-algorithm/STL/aglo/CMakeFiles/STLSample.dir/main.cpp.o"
+  "/home/ubuntu/Practice-algorithm/STL/aglo/TemplateClass.cpp" "/home/ubuntu/Practice-algorithm/STL/aglo/CMakeFiles/STLSample.dir/TemplateClass.cpp.o"
+  "/home/ubuntu/Practice-algorithm/STL/aglo/Tool.cpp" "/home/ubuntu/Practice-algorithm/STL/aglo/CMakeFiles/STLSample.dir/Tool.cpp.o"
+  "/home/ubuntu/Practice-algorithm/STL/aglo/main.cpp" "/home/ubuntu/Practice-algorithm/STL/aglo/CMakeFiles/STLSample.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
