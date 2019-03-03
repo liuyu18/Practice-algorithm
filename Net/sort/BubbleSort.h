8484fba203d98d11d56fb60146568ec5daddf919
Yu <574300833@qq.com>
2019-03-03 20:36:25 +0800

    练习

---
+++ Net/sort/BubbleSort.h
@@ -0,0 +1,12 @@
+#pragma once
+class BubbleSort
+{
+public:
+	BubbleSort();
+	~BubbleSort();
+	void Sort1(int a[], int length);
+	void QSort(int arr[], int n, int low, int hight);
+	void Sort3(int a[], int length);
+	void Sort4(int a[], int length);
+};
+
